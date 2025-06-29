5d4009fdafffcf0444dd79fdd1f6598c6ebe044e
PhanTienDung-BIT230111 <phandung2k5@gmail.com>
2025-06-29 17:35:31 +0700

    v3

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -12,19 +12,5 @@ router.get('/edit/:id',userController.getEditUserPage)
 router.post('/edit/:id',userController.editUser)
 router.post('/delete/:id',userController.deleteUser)
 
-// router.post("/delete/:id" ,async (req,res) => {
-//     try{
-//         await prisma.user.delete({
-//         where: {user_id: parseInt(req.params.id)}
-//     })
-//     res.redirect('/')
-//     }
-//     catch(err){
-//         console.log('Something went wrong')
-//     }
-//     finally{
-//         await prisma.$disconnect();
-//     }
-// })
 
 module.exports = router;
